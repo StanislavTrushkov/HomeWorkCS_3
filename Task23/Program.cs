eddf0fc71c3b627381a3c9cc61c2c01aef7d748b
stanislav <stas09091994@gmail.com>
2022-12-06 20:54:18 +0300

    The homework file is working

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+using System;
+{
+Console.Write("Введите число: ");
+int NumCube = Convert.ToInt32(Console.ReadLine());
+
+void cube(int[] NumCube)
+{
+    int counter = 0;
+    int length = NumCube.Length;
+    while (counter <  length)
+    {
+        NumCube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    while(index < count)
+    {
+        Console.Write(coll[index]+ " ");
+        index++;
+    }
+} 
+
+int[] array = new int[NumCube+1];
+cube(array);
+PrintArray(array);
+Console.ReadKey();
+}
